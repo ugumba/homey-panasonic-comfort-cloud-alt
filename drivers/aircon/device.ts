9321320c1d87409a32478cb0fd3eaeb35924d672
Jarl Larsen <jarl.larsen@gmail.com>
2025-07-27 20:22:38 +0200

    Add setting per device for poll interval in seconds

--- drivers/aircon/device.ts
+++ drivers/aircon/device.ts
@@ -16,6 +16,7 @@ export class MyDevice extends Homey.Device {
   timer: NodeJS.Timer|null = null; 
   alwaysOn: boolean = false;
   fetchMutex:Mutex = new Mutex();
+  fetchinterval: number = 60;
 
   async setCap<T>(name:string, value:T) {
     // Try adding the capability if it does not exist
@@ -171,7 +172,7 @@ export class MyDevice extends Homey.Device {
           this.error("Timer-based fetchFromService failed:", e);
           // Don't set warning here as it would spam the user
         }
-      }, 60000);
+      }, this.fetchinterval * 1000);
     });
   }
 
@@ -259,6 +260,10 @@ export class MyDevice extends Homey.Device {
       3000
     );
 
+    const settings = this.getSettings();
+    this.alwaysOn = settings.alwayson;
+    this.fetchinterval = settings.fetchinterval;
+
     try {
       await this.fetchAndRestartTimer();
     }
@@ -270,15 +275,13 @@ export class MyDevice extends Homey.Device {
     }
 
     // TO BE DEPRECATED: Do not initialize action cards from the device (since devices::onInit is called for every device) but from drivers::onInit
+    // Make sure action cards are initialized only once in case of multiple devices
     await this.driver.actionCardsMutex.runExclusive(async () => {
       if (this.driver.actionCardsInitiated === false) {
         await this.initActionCards();
         this.driver.actionCardsInitiated = true;
       }
     });
-
-    const settings = this.getSettings();
-    this.alwaysOn = settings.alwayson;
 
     this.log("Device '"+this.id+"' has been initialized");
   }
@@ -307,6 +310,10 @@ export class MyDevice extends Homey.Device {
     if (changedKeys.toString().includes('alwayson')) {
       this.alwaysOn = Boolean(newSettings.alwayson);
       this.log("    alwayson changed to: ", this.alwaysOn);
+    }
+    if (changedKeys.toString().includes('fetchinterval')) {
+      this.fetchinterval = Number(newSettings.fetchinterval);
+      this.log("    fetchinterval changed to: ", this.fetchinterval);
     }
   }
 
